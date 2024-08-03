36b2f147319e16ac289d44d70be36a3180e7ecbc
LimDae94 <piay801@naver.com>
2024-08-03 14:44:52 +0900

    (#1)🎉INIT: (Begin a project) 프로젝트 시작
    스프링 부트 프로젝트를 오늘날 기준으로 최신 버전으로 3.3.2로 생성한다. gitignore는 아직 기본 그대로이고, dependency는 당장 필요한 것들만 미리 준비했다. 필요하다면 추가하거나 변경하자.
    
    - https://start.spring.io/
    
    JAVA 21 / Gradle-Groovy / Jar
    
    의존성
    - Spring Web
    - Spring Data JPA
    - Lombok
    - H2
    - MySQL
    - Spring Security
    - Validation

---
+++ src/test/java/com/house/pigeon/PigeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.house.pigeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
