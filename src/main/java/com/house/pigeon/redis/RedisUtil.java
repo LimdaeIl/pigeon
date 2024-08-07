0753183bc3bd987745507289fae62d2c779ea6e4
LimDae94 <piay801@naver.com>
2024-08-07 12:11:45 +0900

    (#15)♻️REF: 매개변수 타입 변환하기
    자바 long 타입에서 Long 타입으로 변환했다. Long 타입을 사용하면 null 값을 허용할 수 있으며, 더 유연한 코드를 작성할 수 있다.
    
    물론, long 타입이 훨씬 메모리 사용도 적고 성능이 좋다. 그러나 이 정도는 미미하다고 판단했다.

--- src/main/java/com/house/pigeon/redis/RedisUtil.java
+++ src/main/java/com/house/pigeon/redis/RedisUtil.java
@@ -22,7 +22,7 @@ public class RedisUtil {
         valueOperations.set(key, value);
     }
 
-    public void setDataExpire(String key, String value, long duration) {
+    public void setDataExpire(String key, String value, Long duration) {
         ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
         Duration expireDuration = Duration.ofSeconds(duration);
         valueOperations.set(key, value, expireDuration);
