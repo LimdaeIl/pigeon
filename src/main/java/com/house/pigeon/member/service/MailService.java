86803c419bcb7dfe10793f6a85c791e147b39325
LimDae94 <piay801@naver.com>
2024-08-07 18:37:55 +0900

    (#17)✨ENH: MailService 안에 Transactional 추가

--- src/main/java/com/house/pigeon/member/service/MailService.java
+++ src/main/java/com/house/pigeon/member/service/MailService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
