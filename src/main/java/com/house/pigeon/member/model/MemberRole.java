f083602a1d76adee8c46e1740cd591896c1e5735
LimDae94 <piay801@naver.com>
2024-08-07 18:37:26 +0900

    (#17)✨ENH: 회원권한 단건 조회, 수정 구현하기
    가장 핵심은 무결성과 일관성을 갖도록 연관 관계 메서드를 구현하는 것이다. 일대다, 다대일에 해당하는 회원과 권한에는 추가, 삭제 메서드와 함께 연관관계 엔티티로부터 각 자신의 필드를 수정하는 로직이 담겨져 있다.
    
    그리고 연관관계 메서드 안에는 set 메서드 안에 수정하는 값이 처리를 적절하게 수행할 수 있도록 작성했다.
    
    정라하자면 회원 권한 수정 요청에 따라 데이터베이스에서 일관성 있는 처리를 수행하고, 자바 코드 안에서도 각 필드마다 일관성과 무결성을 가질 수 있도록 동일한 처리를 수행하는 메서드를 구현했다.

--- src/main/java/com/house/pigeon/member/model/MemberRole.java
+++ src/main/java/com/house/pigeon/member/model/MemberRole.java
@@ -2,10 +2,7 @@ package com.house.pigeon.member.model;
 
 import com.house.pigeon.role.model.Role;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,15 +14,41 @@ public class MemberRole {
     @Column(name = "member_role_id", unique = true, nullable = false, updatable = false)
     private Long id;
 
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
     private Member member;
 
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
     private Role role;
 
     @Builder
     public MemberRole(Member member, Role role) {
         this.member = member;
         this.role = role;
+    }
+
+    public void setMember(Member member) {
+        // 기존 member와의 관계를 제거
+        if (this.member != null) {
+            this.member.getMemberRoles().remove(this);
+        }
+        this.member = member;
+        if (member != null && !member.getMemberRoles().contains(this)) {
+            member.getMemberRoles().add(this);
+        }
+    }
+
+    public void setRole(Role role) {
+        // 기존 role과의 관계를 제거
+        if (this.role != null) {
+            this.role.getMemberRoles().remove(this);
+        }
+        this.role = role;
+        if (role != null && !role.getMemberRoles().contains(this)) {
+            role.getMemberRoles().add(this);
+        }
     }
 }
--- src/main/java/com/house/pigeon/member/repository/MemberRoleRepository.java
+++ src/main/java/com/house/pigeon/member/repository/MemberRoleRepository.java
@@ -1,7 +1,13 @@
 package com.house.pigeon.member.repository;
 
+import com.house.pigeon.member.model.Member;
 import com.house.pigeon.member.model.MemberRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MemberRoleRepository extends JpaRepository<MemberRole, Long> {
+    List<MemberRole> findByMemberId(Long memberId);
+    List<MemberRole> findByMember(Member member);
 }
